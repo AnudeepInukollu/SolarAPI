3993444289a6ce42694bc43acd8efcc80bad0158
AnudeepInukollu <45807849+AnudeepInukollu@users.noreply.github.com>
2018-12-29 21:23:03 +0530

    Add files via upload

---
+++ Solar API/SolarManagement/src/com/citi/tcs/UserPermissions.java
@@ -0,0 +1,21 @@
+package com.citi.tcs;
+
+public class UserPermissions {
+	
+	 private String structuralPermit;
+	 private String electricalPermit;
+	public String getStructuralPermit() {
+		return structuralPermit;
+	}
+	public void setStructuralPermit(String structuralPermit) {
+		this.structuralPermit = structuralPermit;
+	}
+	public String getElectricalPermit() {
+		return electricalPermit;
+	}
+	public void setElectricalPermit(String electricalPermit) {
+		this.electricalPermit = electricalPermit;
+	}
+	 
+
+}
